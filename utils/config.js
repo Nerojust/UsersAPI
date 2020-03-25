0dd8ed18019ae9d334d82b7a1d329accab27eb79
Nyerhovwo Adjekughene <nerojust4@gmail.com>
2020-03-25 15:23:05 +0100

    changed password

--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,8 @@ const mysql = require("mysql");
 //connect to mysql
 var connect = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "",
+  user: "nerojust",
+  password: "Matthew123@",
   database: "NodeJS_DB"
 });
 connect.connect();
